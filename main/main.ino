1bbf99b466de1030c95f351f1e1115db10807ddf
Raj Patel <rpatelpj@gmail.com>
2020-04-06 14:40:21 -0400

    Add Arduino test without LIDAR code

--- main/main.ino
+++ main/main.ino
@@ -6,10 +6,23 @@ Lidar lidar; // TODO: see if this still works if global instead of re-initializi
 
 void setup() {
     // Communicate availability to WiFi module
-    pinMode(?, OUTPUT); // TODO: from PCB, which Arduino pin is connected to WiFi module IO2
+    pinMode(2, OUTPUT); // TODO: from PCB, which Arduino pin is connected to WiFi module IO2
+
+    // Test without LIDAR
+    // Serial.begin(9600);
 }
 
 void loop() {
     // Transmit availability to WiFi module
     digitalWrite(?, lidar.getDistance(Lidar::METER) < SPOT_DISTANCE);  // TODO: from PCB, which Arduino pin is connected to WiFi module IO2
+
+    // Test without LIDAR
+    // if (Serial.available() > 0) {
+    //     int availability = Serial.read();
+    //     if (availability == '0') {
+    //         digitalWrite(2, LOW);
+    //     } else if (availability == '1') {
+    //         digitalWrite(2, HIGH);
+    //     }
+    // }
 }
